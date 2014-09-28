ed82b3bf361135c75f4526e94d5b7c6ce4715301
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2014-09-28 13:19:27 -0430

    Moved some msgid from plone domain to collective.vaporisation domain

--- collective/vaporisation/i18n.sh
+++ collective/vaporisation/i18n.sh
@@ -2,10 +2,10 @@
 
 DOMAIN='collective.vaporisation'
 
-i18ndude rebuild-pot --pot ./locales/${DOMAIN}.pot --create ${DOMAIN} .  || exit 1
+i18ndude rebuild-pot --pot ./locales/${DOMAIN}.pot --merge ./locales/manual.pot --create ${DOMAIN} .  || exit 1
 i18ndude sync --pot ./locales/${DOMAIN}.pot ./locales/*/LC_MESSAGES/${DOMAIN}.po
 
-i18ndude rebuild-pot --pot ./locales/plone.pot --merge ./locales/plone-manual.pot --create plone ./profiles/default  || exit 1
+i18ndude rebuild-pot --pot ./locales/plone.pot --create plone ./profiles/default  || exit 1
 i18ndude sync --pot ./locales/plone.pot ./locales/*/LC_MESSAGES/plone.po
 
 WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
